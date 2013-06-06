05c571be2e4a5e0beb8b32bfeebec215d9bb6fd0
Sam King <samking@cs.stanford.edu>
2013-06-05 23:15:44 -0700

    added way to highlight current tab in terminal

---
+++ fix-terminal-tab-color.bash
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Modified from http://askubuntu.com/a/252192/66569
+# Make sure to log out and log in for the changes to take effect!
+# You can change the CSS in sample-gtk.css to change the color.
+cp sample-gtk.css ~/.config/gtk-3.0/gtk.css
